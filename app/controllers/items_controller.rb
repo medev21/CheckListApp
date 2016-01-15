0d47024e564d9c86b0d57be9c8a872cd51d89043
Martin Ben <mebenav88@gmail.com>
2016-01-15 14:44:49 -0600

    convert erb files to slim, add navbar header in index view, display item in index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
