7789c0a574db659a92ecacf2f47ee4adb93243a1
Martin Ben <mebenav88@gmail.com>
2016-01-15 12:59:25 -0600

    add form to create items, add item new create controllers and view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
   end
-  
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params_item)
+    if @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def params_item
+      params.require(:item).permit(:title, :description)
+    end
+
+    def find_item
+      @item = Item.find(params[:id])
+    end
 end
